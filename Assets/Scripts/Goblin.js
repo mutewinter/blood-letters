29a714d209fa39909f7ea88cdbeecfce4c6667c2
Jeremy Mack <jeremy@pileofturtles.com>
2015-07-27 09:23:06 -0500

    feat: blinking red goblin

---
+++ Assets/Scripts/Goblin.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D(other: Collider2D) {
+  var r = GetComponent.<Renderer>();
+  var originalColor = r.material.color;
+  r.material.color = Color.red;
+  yield WaitForSeconds(.5);
+  r.material.color = originalColor;
+}
