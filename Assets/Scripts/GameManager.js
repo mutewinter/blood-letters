6f8d42d7ccb8907b9a06b6843684724e012e156e
Jeremy Mack <jeremy@pileofturtles.com>
2015-07-27 09:39:10 -0500

    feat: random goblins

--- Assets/Scripts/GameManager.js
+++ Assets/Scripts/GameManager.js
@@ -1,7 +1,19 @@
 ﻿#pragma strict
 
+public var enemyPrefabs : GameObject[];
+
 function Start () {
   Debug.Log('Game on');
+
+  var goblin = enemyPrefabs[0];
+  for (var i = 0; i < 8; i++) {
+    var position = new Vector3(
+      Random.Range(-2.0F, 2.0F),
+      Random.Range(-2.0F, 2.0F),
+      0
+    );
+    Instantiate(goblin, position, Quaternion.identity);
+  }
 }
 
 function Update () {
