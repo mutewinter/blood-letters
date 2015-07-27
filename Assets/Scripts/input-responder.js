8a43836c4d179d6b5537b5d5d5f268b188693fd8
Jeremy Mack <jeremy@pileofturtles.com>
2015-07-27 09:04:03 -0500

    feat: click and hold to move!

---
+++ Assets/Scripts/input-responder.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var moveSpeed = 2.0;  // Units per second
+
+function Start () {
+}
+
+function Update () {
+  if (Input.GetMouseButton(0)) {
+    var targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    targetPos.z = transform.position.z;
+
+    transform.position = Vector2.MoveTowards(
+      transform.position, targetPos, moveSpeed*Time.deltaTime
+    );
+  }
+}
