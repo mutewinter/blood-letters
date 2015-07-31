7df1e5a8c6a2d8308a17e8f7a583e3126db9d314
Jeremy Mack <jeremy@pileofturtles.com>
2015-07-31 09:56:44 -0500

    feat: popup damage

--- Assets/Scripts/Bullet.js
+++ Assets/Scripts/Bullet.js
@@ -1,8 +1,11 @@
 ﻿#pragma strict
 
+import System.Collections.Generic;
+
 public var player: Player;
 public var damage = 0;
 public var secondsToLive = 0.5;
+public var DamageText: GameObject;
 
 function Start () {
   yield WaitForSeconds(secondsToLive);
@@ -16,9 +19,25 @@ function Update () {
 function hit(other: Character) {
   other.health -= damage;
   Debug.Log('Hit ' + other + ' for ' + damage + '. ' + 'HP: ' + other.health);
+
+  yield showPopupDamage(damage, other.transform.position);
+
   if (other.health <= 0) {
     player.gainExperience(other.worthExperience);
     player.kills++;
   }
   Destroy(gameObject);
 }
+
+function showPopupDamage(amount, target: Vector3) {
+  var textPosition = target;
+  textPosition.y += 0.3;
+  var damageText = Instantiate(
+    DamageText, textPosition, Quaternion.identity
+  );
+  var textMesh = damageText.GetComponent.<TextMesh>();
+  textMesh.text = String.Format('-{0}', amount);
+
+  return void;
+}
+
---
+++ Assets/Scripts/DamageText.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+public var secondsToShow = 0.5;
+
+function Start () {
+  yield WaitForSeconds(secondsToShow);
+  Destroy(gameObject);
+}
+
+function Update () {
+
+}
