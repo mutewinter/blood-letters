e3c2acdcc06589fa4a8a9d45d369f17675f22a76
Jeremy Mack <jeremy@pileofturtles.com>
2015-08-01 06:09:34 -0500

    refactor: instantiate player instead of relying on scnee

--- Assets/Scripts/GameManager.js
+++ Assets/Scripts/GameManager.js
@@ -1,8 +1,11 @@
 ﻿#pragma strict
 
 public var enemyPrefabs : GameObject[];
+public var player : GameObject;
 
 function Start () {
+  Instantiate(player, Vector3.zero, Quaternion.identity);
+
   var goblin = enemyPrefabs[0];
   for (var i = 0; i < 8; i++) {
     var position = new Vector3(
