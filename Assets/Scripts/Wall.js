014fd20f62163963a7cde7245c14b33a9ac0b622
Jeremy Mack <jeremy@pileofturtles.com>
2015-08-08 09:57:49 -0400

    feat: bounce projectiles and enemies off walls

--- Assets/Scripts/Wall.js
+++ Assets/Scripts/Wall.js
@@ -8,19 +8,27 @@ function Update () {
 
 }
 
-function stopCollider2D(other: Collider2D) {
+function reflectCollider2D(other: Collider2D) {
   var rigidbody2D = other.GetComponentInParent.<Rigidbody2D>();
   if (rigidbody2D) {
-    rigidbody2D.velocity = Vector2.zero;
-    rigidbody2D.angularVelocity = 0;
+    // Hack to determine angle of wall.
+    // TODO we should use some Quaternion functions to determine the angle
+    // with more precision. This setup only supports vertical and horizontal
+    // walls.
+    if (transform.rotation.w < 1) {
+      rigidbody2D.velocity = Vector2.Reflect(rigidbody2D.velocity, Vector2.up);
+    } else {
+      rigidbody2D.velocity = Vector2.Reflect(
+        rigidbody2D.velocity, Vector2.right
+      );
+    }
   }
-  // TODO send force in opposite direction away from wall
 }
 
 function OnTriggerEnter2D(other: Collider2D) {
-  stopCollider2D(other);
+  reflectCollider2D(other);
 }
 
 function OnTriggerStay2D(other: Collider2D) {
-  stopCollider2D(other);
+  reflectCollider2D(other);
 }
