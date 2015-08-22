04d49b92bdd817ce293f054df00221c7aba29f26
Jeremy Mack <jeremy@pileofturtles.com>
2015-08-22 11:19:34 -0500

    fix: make kobolds a little less crazy difficult

--- Assets/Scripts/Characters/Kobold.js
+++ Assets/Scripts/Characters/Kobold.js
@@ -1,9 +1,11 @@
 ﻿#pragma strict
 
 class Kobold extends Character {
+  public var accuracy = 0.5;
+
   private var nextFire : float = 0;
   private var bulletSpeed : float = 100;
-  private var fireRate : float = 0.8;
+  private var fireRate : float = 1.6;
   private var playerFinder: PlayerFinder;
 
   function Start() {
@@ -25,7 +27,10 @@ class Kobold extends Character {
       // Force direction is zero when the player can't be found or the player
       // is at the start position. Don't do anothing in this case.
       if (forceDirection == Vector2.zero) { return; }
-      attack(forceDirection);
+      var fudge = 2f - (accuracy * 2f);
+      forceDirection.x += Random.Range(-fudge, fudge);
+      forceDirection.y += Random.Range(-fudge, fudge);
+      attack(forceDirection.normalized);
     }
   }
 }
